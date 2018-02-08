69fcc91622dc393d85039ca1310716e299bae3fe
Владислав Прекель <79232809414@yandex.ru>
2018-02-09 00:08:03 +0700

    eps

--- MyExpression.Console/Program.cs
+++ MyExpression.Console/Program.cs
@@ -56,7 +56,7 @@ namespace MyExpression.Console
 			
 			var s = System.Console.ReadLine();
 			var p = Polynomial.Parse(s);
-			var e = new PolynomialEquation(p);
+			var e = new PolynomialEquation(p, 1e-8);
 			e.Solve();
 			System.Console.WriteLine(String.Join(" ", e.Roots));
 		}
