54712e6bf261987d18d9f2aa99883fdd52312ca8
Владислав Прекель <79232809414@yandex.ru>
2018-02-13 20:26:56 +0700

    tests

--- MyExpression.Core.Tests/MonomialTests.cs
+++ MyExpression.Core.Tests/MonomialTests.cs
@@ -17,7 +17,44 @@ namespace MyExpression.Core.Tests
 		public void SumTest_Random()
 		{
 			var r = new MyRandom();
-			var a = r.NextDouble() * r.Next() * r.NextSign();
+			var a1 = r.NextDouble() * r.Next() * r.NextSign();
+			var b = r.Next(1, 100);
+			var a2 = r.NextDouble() * r.Next() * r.NextSign();
+
+			var m1 = new Monomial(a1, b);
+			var m2 = new Monomial(a2, b);
+
+			var me = new Monomial(a1 + a2, b);
+
+			var ms = m1 + m2;
+
+			Assert.AreEqual(me, ms);
 		}
+
+		[Test]
+		public void SumSubMultTest_Random()
+		{
+			var r = new MyRandom();
+			var a1 = r.NextDouble() * r.Next() * r.NextSign();
+			var b = r.Next(1, 100);
+			var a2 = r.NextDouble() * r.Next() * r.NextSign();
+
+			var m1 = new Monomial(a1, b);
+			var m2 = new Monomial(a2, b);
+
+			var me1 = new Monomial(a1 + a2, b);
+			var ms1 = m1 + m2;
+
+			var me2 = new Monomial(a1 - a2, b);
+			var ms2 = m1 - m2;
+
+			var me3 = new Monomial(a1 * a2, b + b);
+			var ms3 = m1 * m2;
+
+			Assert.AreEqual(me1, ms1);
+			Assert.AreEqual(me2, ms2);
+			Assert.AreEqual(me3, ms3);
+		}
+
 	}
 }
