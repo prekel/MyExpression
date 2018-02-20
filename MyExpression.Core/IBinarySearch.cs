bc4729f9f697bd7a2c34abd3a9be1d96266d082e
Владислав Прекель <79232809414@yandex.ru>
2018-02-20 17:34:11 +0700

    IBinarySearch

---
+++ MyExpression.Core/IBinarySearch.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2018 Vladislav Prekel
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExpression.Core
+{
+	interface IBinarySearch
+	{
+		double StartLeft { get; }
+		double StartRight { get; }
+		double Left { get; }
+		double Right { get; }
+		double Epsilon { get; }
+		double Answer { get; }
+		double Solve();
+	}
+}
---
+++ MyExpression.Core/RecursiveBinarySearch.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) 2018 Vladislav Prekel
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExpression.Core
+{
+	public class RecursiveBinarySearch : IBinarySearch
+	{
+		public double StartLeft { get; private set; }
+		public double StartRight { get; private set; }
+		public double Left { get; private set; }
+		public double Right { get; private set; }
+		public double Epsilon { get; private set; }
+		public double Answer { get; private set; }
+
+		public RecursiveBinarySearch(double l, double r, double eps)
+		{
+			StartLeft = l;
+			Left = l;
+			StartRight = r;
+			Right = r;
+			Epsilon = eps;
+		}
+
+		public double Solve()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
