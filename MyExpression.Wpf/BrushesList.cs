e86300d58a26fb7da42da2ab0121486ff33cba74
Владислав Прекель <79232809414@yandex.ru>
2018-02-21 23:07:22 +0700

    получение кистей из Brushes для BrushesList с помощью рефлексии

---
+++ MyExpression.Wpf/BrushesList.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) 2018 Vladislav Prekel
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection;
+
+using MyExpression.Core;
+
+namespace MyExpression.Wpf
+{
+	public class BrushesList : ComboBox
+	{
+		private static List<(string, SolidColorBrush)> Brushes { get; set; } = new List<(string, SolidColorBrush)>(141);
+		static BrushesList()
+		{
+			var t = typeof(Brushes);
+			var p = t.GetProperties();
+			foreach (var i in p)
+			{
+				Brushes.Add((i.Name, (SolidColorBrush)i.GetValue(null)));
+			}
+		}
+		public BrushesList() : base()
+		{
+		}
+	}
+}
