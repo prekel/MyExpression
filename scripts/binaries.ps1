118a40f6a3130660d7540200eb5312223c64e201
Владислав Прекель <misterptits@yandex.ru>
2018-03-01 22:34:07 +0700

    сбор бинарников через скрипт

---
+++ scripts/binaries.ps1
@@ -0,0 +1,7 @@
+﻿New-Item -Path 'bin' -ItemType "directory" 
+$bins = "MyExpression.Core.dll", "MyExpression.Console.exe", "MyExpression.Wpf.exe"
+foreach ($i in $bins)
+{
+    $j = $i.Substring(0, $i.Length - 4)
+    Copy-Item -Path "$j\bin\Release\$i" -Destination "bin\$i"
+}
