bb9af87343919dcaaffc25ef88f64623e19880fb
Vladislav Prekel <misterptits@yandex.ru>
2021-01-16 00:25:40 +0700

    Something

---
+++ MyExpression.FSharp.Console/Program.fs
@@ -0,0 +1,20 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+open MyExpression.FSharp.Core.Core
+
+// Define a function to construct a message to print
+let from whom = sprintf "from %s" whom
+
+[<EntryPoint>]
+let main argv =
+    let message = from "F#" // Call the function
+    printfn "Hello world %s" message
+
+    let y =
+        { Left = -1.0; Right = 4.0 }
+        |> isInInterval 3.0
+
+    printfn "%A" y
+    
+    0 // return an integer exit code
--- MyExpression.FSharp.Core/Library.fs
+++ MyExpression.FSharp.Core/Library.fs
@@ -1,5 +1,27 @@
 ﻿namespace MyExpression.FSharp.Core
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open System
+
+module Core =
+
+    type Monomial = { Coefficient: float; Degree: float }
+
+    type Polynomial = Monomial list
+
+    type Interval = { Left: float; Right: float }
+
+    let isInInterval x interval =
+        x >= interval.Left && x <= interval.Right
+
+
+    type PolynomialEquation =
+        { Polynomial: Polynomial
+          Epsilon: float }
+
+    type Derivativable = Monomial  | Polynomial
+    
+//    let derivative x =
+//        match x with
+//        | Monomial(y) -> y
+//        | Polynomial -> failwith ""
+            
