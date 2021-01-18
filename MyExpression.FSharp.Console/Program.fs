65be0a452919083cbfa6619cd3a883174ae1047a
Vladislav Prekel <misterptits@yandex.ru>
2021-01-18 20:38:15 +0700

    dasdasd

--- MyExpression.FSharp.Console/Program.fs
+++ MyExpression.FSharp.Console/Program.fs
@@ -12,9 +12,20 @@ let main argv =
     printfn "Hello world %s" message
 
     let y =
-        { Left = -1.0; Right = 4.0 }
-        |> isInInterval 3.0
+        { Left = -1.0; Right = 4.0 } |> isInInterval 3.0
 
     printfn "%A" y
-    
+
+    let mon = { Coefficient = 3.0; Degree = 2 }
+
+    let d = derivative (Monomial mon)
+
+
+    let p =
+        Polynomial [ mon
+                     monomial 12.3 3
+                     monomial 2.0 4 ]
+
+    let pd = derivative p
+
     0 // return an integer exit code
--- MyExpression.FSharp.Core/Library.fs
+++ MyExpression.FSharp.Core/Library.fs
@@ -4,7 +4,8 @@ open System
 
 module Core =
 
-    type Monomial = { Coefficient: float; Degree: float }
+    type Monomial = { Coefficient: float; Degree: int }
+
 
     type Polynomial = Monomial list
 
@@ -13,15 +14,26 @@ module Core =
     let isInInterval x interval =
         x >= interval.Left && x <= interval.Right
 
+    let monomial coef deg = { Coefficient = coef; Degree = deg }
 
     type PolynomialEquation =
         { Polynomial: Polynomial
           Epsilon: float }
 
-    type Derivativable = Monomial  | Polynomial
-    
-//    let derivative x =
-//        match x with
-//        | Monomial(y) -> y
-//        | Polynomial -> failwith ""
-            
+    type Derivativable =
+        | Monomial of Monomial
+        | Polynomial of Polynomial
+
+    let rec derivative x =
+        match x with
+        | Monomial (y) ->
+            Monomial
+                { Coefficient = y.Coefficient * float y.Degree
+                  Degree = y.Degree - 1 }
+        | Polynomial (y) ->
+            (Polynomial
+                (y
+                 |> List.map (fun t ->
+                     match derivative (Monomial t) with
+                     | Monomial (u) -> u
+                     | _ -> failwith "")))
