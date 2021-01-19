7e5044b90ce5001886549249bff4898a03427997
Vladislav Prekel <misterptits@yandex.ru>
2021-01-19 15:25:15 +0700

    Started monotony intervals

--- MyExpression.FSharp.Core/Interval.fs
+++ MyExpression.FSharp.Core/Interval.fs
@@ -7,3 +7,26 @@ module Interval =
 
     let isInInterval x interval =
         x >= interval.Left && x <= interval.Right
+
+    let infinityInterval = create -infinity infinity
+
+    let positiveInfinityInterval a = create -infinity a
+
+    let negativeInfinityInterval a = create a infinity
+
+    let ofList roots =
+        match roots with
+        | [] -> [ infinityInterval ]
+        | [ a ] ->
+            [ negativeInfinityInterval a
+              positiveInfinityInterval a ]
+        | [ a; b ] ->
+            [ negativeInfinityInterval a
+              create a b
+              positiveInfinityInterval b ]
+        | [ a; b; c ] ->
+            [ negativeInfinityInterval a
+              create a b
+              create b c
+              positiveInfinityInterval c ]
+        | _ -> notImplemented ""
