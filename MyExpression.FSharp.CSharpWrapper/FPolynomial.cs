724d0e81c36c1a95434bd7854f6e31f2bfde1d87
Vladislav Prekel <misterptits@yandex.ru>
2021-01-20 13:18:50 +0700

    FPolynomial

--- MyExpression.FSharp.CSharpWrapper/FPolynomial.cs
+++ MyExpression.FSharp.CSharpWrapper/FPolynomial.cs
@@ -2,28 +2,43 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.FSharp.Collections;
 
 using MyExpression.Core;
+using MyExpression.FSharp.Core;
 
+using Monomial = MyExpression.FSharp.Core.Monomial;
 
 namespace MyExpression.FSharp.CSharpWrapper
 {
     public class FPolynomial : IPolynomial
     {
-        public double Calculate(double x) => throw new System.NotImplementedException();
+        public double Calculate(double x) => PolynomialModule.calc(_polynomial, x);
 
-        public IFunctionX Derivative { get; }
-        public IEnumerator<IMonomial> GetEnumerator() => throw new System.NotImplementedException();
+        public IFunctionX Derivative => new FPolynomial(PolynomialModule.derivative(_polynomial));
+
+        public IEnumerator<IMonomial> GetEnumerator() =>
+            _polynomial.Select(u => new FMonomial(u.Coefficient, u.Degree)).GetEnumerator();
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-        
 
-        public FPolynomial(IPolynomial a)
+        private readonly FSharpList<Monomial> _polynomial;
+
+        private FPolynomial(FSharpList<Monomial> polynomial) => _polynomial = polynomial;
+
+        public FPolynomial(IEnumerable<IMonomial> a)
         {
+            _polynomial = PolynomialModule.ofList(
+                ToFSharpList(new List<IMonomial>(a)
+                    .Select(u => MonomialModule.create(u.Coefficient, (int) u.Degree)).ToList()));
         }
 
-        public FPolynomial(params double[] v)
-        {
-        }
+        private static FSharpList<T> ToFSharpList<T>(IList<T> input) => CreateFSharpList(input, 0);
+
+        private static FSharpList<T> CreateFSharpList<T>(IList<T> input, int index) => index >= input.Count
+            ? FSharpList<T>.Empty
+            : FSharpList<T>.Cons(input[index], CreateFSharpList(input, index + 1));
     }
 }
