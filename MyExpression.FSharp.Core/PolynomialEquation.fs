a7fb81e70cd146ecd1f676f6f3c03e7e964e834b
Vladislav Prekel <misterptits@yandex.ru>
2021-01-22 17:33:49 +0700

    let private filterIntervals

--- MyExpression.FSharp.Core/PolynomialEquation.fs
+++ MyExpression.FSharp.Core/PolynomialEquation.fs
@@ -3,23 +3,21 @@
 namespace MyExpression.FSharp.Core
 
 module PolynomialEquation =
+    let private filterIntervals eps calc =
+        List.filter (fun (i: Interval) ->
+            match i with
+            | (NegativeInfinity, right) ->
+                let a, b = (calc (right - 1.), calc right)
+                not (a > b && b > eps || a < b && b < -eps)
+            | (left, PositiveInfinity) ->
+                let a, b = (calc left, calc (left + 1.))
+                not (a > b && a < -eps || a < b && a > eps)
+            | (left, right) ->
+                let a, b = (calc left, calc right)
+                not (sign (a) * sign (b) > 0 && abs (a * b) >= eps))
 
     let solve eps poly =
         let rec solveRec poly =
-            let calc = Polynomial.calc poly
-
-            let filterIntervals =
-                List.filter (function
-                    | (NegativeInfinity, right) ->
-                        let a, b = (calc (right - 1.), calc right)
-                        not (a > b && b > eps || a < b && b < -eps)
-                    | (left, PositiveInfinity) ->
-                        let a, b = (calc left, calc (left + 1.))
-                        not (a > b && a < -eps || a < b && a > eps)
-                    | (left, right) ->
-                        let a, b = (calc left, calc right)
-                        not (sign (a) * sign (b) > 0 && abs (a * b) >= eps))
-
             match poly |> Polynomial.degree with
             | 1 ->
                 poly
@@ -27,14 +25,15 @@ module PolynomialEquation =
                 |> LinearEquation.solve
                 |> List.singleton
             | _ ->
+                let calc = Polynomial.calc poly
+
                 poly
                 |> Polynomial.derivative
                 |> solveRec
                 |> Interval.intervalsOfList
-                |> filterIntervals
+                |> filterIntervals eps calc
                 |> List.map (BinarySearch.search calc eps)
                 |> List.sort
-
 
         poly |> Polynomial.normalize |> solveRec
 
