9efd70dfb9b975fca58b1ff2b39021bec09c8db9
Vladislav Prekel <misterptits@yandex.ru>
2021-01-24 22:49:55 +0700

    fix global for fable

--- MyExpression.FSharp.Feliz/src/App.fs
+++ MyExpression.FSharp.Feliz/src/App.fs
@@ -2,5 +2,16 @@ module App
 
 open Feliz
 
+open MyExpression.FSharp.Core
+
 [<ReactComponent>]
-let HelloWorld() = Html.h1 "Hello world"
+let HelloWorld () =
+    Html.h1
+        ([ Monomial.create 1. 3
+           Monomial.create -2. 2
+           Monomial.create -1. 1
+           Monomial.create 2. 0 ]
+         |> Polynomial.ofList
+         |> Polynomial.normalize
+         |> PolynomialEquation.solve 1e-4
+         |> sprintf "%A")
